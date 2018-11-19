35da2cadf1ad7063503d1c2619e584e7ce543d31
Panchajanya Banerjee <panchajb@usc.edu>
2018-11-18 21:36:52 -0800

    Update run_app.py

--- spotify_bot/run_app.py
+++ spotify_bot/run_app.py
@@ -9,6 +9,6 @@ nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/toptracksnlu')
 action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter, action_endpoint = action_endpoint)
 
-input_channel = SlackInput('xoxb-481205792580-480640272064-0c5QSWf6ER1qhnUjP9eJqCeX')
+input_channel = SlackInput('xoxb-') # Slack Bot User OAuth Access Token here
 
 agent.handle_channels([input_channel], 5004, serve_forever=True)
